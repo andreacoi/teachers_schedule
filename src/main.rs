1bd2ce8087876b7efa03812b1d4f101108981c2d
Andrea Coi <andrea.coi@outlook.it>
2024-10-20 11:42:46 +0200

    riporta file main allo stato originale

--- src/main.rs
+++ src/main.rs
@@ -1,34 +1,4 @@
-use gtk::prelude::*;
-use gtk::{glib, Application, ApplicationWindow, Button};
-
-const APP_ID: &str = "com.teacherschedule.app";
-
-fn main() -> glib::ExitCode {
-    // crea una nuova applicazione
-    let app = Application::builder().application_id(APP_ID).build();
-    app.connect_activate(build_ui);
-    app.run()
+fn main() {
+    println!("Hello World");
 }
 
-fn build_ui(app: &Application) {
-    // crea un pulsante
-    let button = Button::builder()
-        .label("Cliccami!")
-        .margin_top(12)
-        .margin_bottom(12)
-        .margin_start(12)
-        .margin_end(12)
-        .build();
-    // crea il comportamento del pulsante
-    button.connect_clicked(|button| {
-        button.set_label("Good Boy!");
-    });
-    // crea la finestra base
-    let window = ApplicationWindow::builder()
-        .application(app)
-        .title("Creatore tabelle orari docenti")
-        .child(&button)
-        .build();
-    // mostra la finestra
-    window.present();
-}
