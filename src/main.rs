4a3707fb596aba42d941271441fb5b6cd7c7c732
Andrea Coi <andrea.coi@outlook.it>
2024-10-20 17:48:37 +0200

    inserisce nel main le funzioni di inizializzazione del database e delle tabelle

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,15 @@
-fn main() {
-    println!("Hello World");
+mod db {
+    pub mod connessione;
+    pub mod operazioni;
 }
+use db::connessione::Database;
+use db::operazioni::OperazioniPreliminariTabelle;
 
+fn main() -> rusqlite::Result<()> {
+    // inizializza la connessione
+    let db = Database::new()?;
+    // crea le tabelle necessarie al progetto
+    db.inizializza_tabelle()?;
+
+    Ok(())
+}
